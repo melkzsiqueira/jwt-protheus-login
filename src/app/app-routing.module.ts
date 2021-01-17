589e2c72a633c3961d897b3ad56eebb925657346
Melkz Siqueira <melkz.siqueira@gmail.com>
2021-01-17 18:04:26 -0300

    Ajuste na rota de excessão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { AuthenticationGuard } from '../app/guards/authentication/authentication.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent, canActivate: [AuthenticationGuard] },
   { path: 'sign-in', component: SignInComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
